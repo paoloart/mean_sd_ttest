e97b6d23e5213e1440daed8be7b10964d36a5e68
Paolo Artoni <paolo.artoni84@gmail.com>
2022-07-25 03:25:38 +0200

    plt.show at the end

--- media.py
+++ media.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('dataset.csv') # legge il csv e lo salva come dataframe
 
-print(dataset) # printa il dataframe
+#print(dataset) # printa il dataframe
 
 mean1 = dataset.iloc[:,0].mean() # calcola la media dei valori della colonna 1
 mean2 = dataset.iloc[:,1].mean() # calcola la media dei valori della colonna 2
@@ -16,7 +16,7 @@ std2 = dataset.iloc[:,1].std() # calcola la deviazione standard dei valori della
 plt.hist(dataset.iloc[:,0], bins=20, color='red', alpha=0.5, label='colonna 1')
 plt.hist(dataset.iloc[:,1], bins=20, color='blue', alpha=0.5, label='colonna 2')
 plt.legend()
-plt.show()
+
 
 
 
@@ -25,6 +25,7 @@ plt.show()
 t_stat, p_value = stats.ttest_ind(dataset.iloc[:,0], dataset.iloc[:,1]) # calcola il t-statistico e il p-value
 
 #printa i risultati a terminale
-print(std1, std2)
+#print(std1, std2)
 print(mean1, mean2)
-print(t_stat, p_value)
+#print(t_stat, p_value)
+plt.show()
