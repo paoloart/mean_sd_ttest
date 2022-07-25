52e0e4407e3721d04ace0b4cc0d15c81534c1d8e
Paolo Artoni <paolo.artoni84@gmail.com>
2022-07-25 03:18:21 +0200

    add plot

--- media.py
+++ media.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from scipy import stats
+import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('dataset.csv') # legge il csv e lo salva come dataframe
 
@@ -10,6 +11,15 @@ mean2 = dataset.iloc[:,1].mean() # calcola la media dei valori della colonna 2
 
 std1 = dataset.iloc[:,0].std() # calcola la deviazione standard dei valori della colonna 1
 std2 = dataset.iloc[:,1].std() # calcola la deviazione standard dei valori della colonna 2
+
+# crea un istogramma con le due distribuzioni
+plt.hist(dataset.iloc[:,0], bins=20, color='red', alpha=0.5, label='colonna 1')
+plt.hist(dataset.iloc[:,1], bins=20, color='blue', alpha=0.5, label='colonna 2')
+plt.legend()
+plt.show()
+
+
+
 
 
 t_stat, p_value = stats.ttest_ind(dataset.iloc[:,0], dataset.iloc[:,1]) # calcola il t-statistico e il p-value
