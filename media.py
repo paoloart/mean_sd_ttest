ad4768e7014af2f22a720a5fb625d5aeedd30e23
Paolo Artoni <paolo.artoni84@gmail.com>
2022-07-29 00:44:46 +0200

    update

--- media.py
+++ media.py
@@ -2,9 +2,12 @@ import pandas as pd
 from scipy import stats
 import matplotlib.pyplot as plt
 
-dataset = pd.read_csv('dataset.csv') # legge il csv e lo salva come dataframe
+# legge il csv e lo trasforma in dataframe ma non prende la prima colonna come intestazione
+
+dataset = pd.read_csv('dataset.csv', header=None) # legge il csv e lo salva come dataframe
 
 #print(dataset) # printa il dataframe
+print(dataset)
 
 mean1 = dataset.iloc[:,0].mean() # calcola la media dei valori della colonna 1
 mean2 = dataset.iloc[:,1].mean() # calcola la media dei valori della colonna 2
@@ -25,7 +28,8 @@ plt.legend()
 t_stat, p_value = stats.ttest_ind(dataset.iloc[:,0], dataset.iloc[:,1]) # calcola il t-statistico e il p-value
 
 #printa i risultati a terminale
-#print(std1, std2)
+print(std1, std2)
 print(mean1, mean2)
+print(t_stat, p_value)
 #print(t_stat, p_value)
-plt.show()
+
